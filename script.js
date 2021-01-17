d03a38182b8a091f5c6c00559adffb42707c4fc2
GoEun Choi <choi0119@algonquinlive.com>
2021-01-17 11:57:25 -0500

    add again button

--- script.js
+++ script.js
@@ -28,6 +28,10 @@ let score = 20; // it need to be mutable so that it would be let
 //Event liseter : page 에나타는 이벤트를 만들어주는 메또우
 // addEventlistner 만들고 어떤 click 이란 함수를 시행하는 걸 만들거다
 
+// agian ㅎㅏ면 리셋되는거
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
+});
 document
   .querySelector('.check')
   // add event listener function experession 과 비슷 'click'은 이벤트 그냥 이름임, function 이 행동하는 펑션, call할필요 없다. function defined 만 되있음
@@ -41,19 +45,40 @@ document
 
     //숫자가 아닌다른것 입력했을경우
     if (!guess) {
+      document.querySelector('.message').textContent =
+        'error, you need to put right value';
       // 게스가 아닌경우 즉 false
       // 우리가 사용하는 input 은 보통 string 받아오기 때문에 이것을 숫자로 비교할려면 숫자로 만들어야한다 *Number() function 이용
     } else if (guess > secretNumber) {
-      document.querySelector('.message').textContent = `it is lower ${guess}`;
-      score--;
-      document.querySelector('.score').textContent = score;
+      if (score > 1) {
+        // score
+        document.querySelector('.message').textContent = `it is lower ${guess}`;
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = `you lose your game`;
+        document.querySelector('.score').textContent = 0;
+        document.querySelector('.number').textContent = secretNumber;
+      }
     } else if (guess < secretNumber) {
-      document.querySelector('.message').textContent = `it is higher ${guess}`;
-      score--;
-      document.querySelector('.score').textContet = score;
+      if (score > 1) {
+        document.querySelector(
+          '.message'
+        ).textContent = `it is higher ${guess}`;
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = `you lose your game`;
+        [];
+        document.querySelector('.score').textContent = 0;
+        document.querySelector('.number').textContent = secretNumber;
+      }
     } else {
+      //guess right
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.message').textContent = 'you are right';
       document.querySelector('.highscore').textContent = score;
+      document.querySelector('body').style.backgroundColor = 'green';
+      document.querySelector('.number').style.tr;
     }
   });
